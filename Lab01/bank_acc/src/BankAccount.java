5eb12aa0153b420b3dcaa9faa3dd62d071f7e529
Truong Ng Dinh <Truong.ND235566@sis.hust.edu.vn>
2024-10-24 09:48:28 +0700

    update

--- Lab01/bank_acc/src/BankAccount.java
+++ Lab01/bank_acc/src/BankAccount.java
@@ -1,22 +1,27 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 
 public class BankAccount {
     public String name;
     public double balance;
     public double monthlyInterest;
     ArrayList<String> history = new ArrayList<>();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     public double CheckBalance() {
         return balance;
     }
 
-    public void Deposit(double amount, String date) {
+    public void Deposit(double amount, LocalDate date) {
         balance = balance + amount;
         String str_amount = String.valueOf(amount);
-        String[] add = {"Deposit", str_amount, date};
+        String[] add = {"Deposit", str_amount, date.format(formatter)};
         history.add(add[0] + " - " + add[1] + " - " + add[2]);
     }
-    public void Withdraw(double amount, String date) {
+    public void Withdraw(double amount, LocalDate date) {
         balance = balance - amount;
         
     }
--- Lab01/bank_acc/src/Main.java
+++ Lab01/bank_acc/src/Main.java
@@ -1,5 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount acc = new BankAccount();
+        acc.name = "Nguyễn Văn A";
+        acc.balance = 5000000;
+        acc.monthlyInterest = 0.005;
+        System.out.println("Tạo tài khoản mới: " + acc.name);
+        System.out.println("Số dư ban đầu: " + String.valueOf(acc.balance));
+
+        acc.Deposit(1000000, LocalDate.now());
     }
 }
