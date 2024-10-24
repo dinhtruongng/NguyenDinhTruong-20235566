e9aaf25b1774306a10b8cbbdece99f4febfbaa7a
Truong Ng Dinh <Truong.ND235566@sis.hust.edu.vn>
2024-10-24 10:19:13 +0700

    update

--- Lab01/bank_acc/src/BankAccount.java
+++ Lab01/bank_acc/src/BankAccount.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-
+import java.util.List;
+import java.time.Month;
 
 public class BankAccount {
     public String name;
     public double balance;
     public double monthlyInterest;
     ArrayList<String> history = new ArrayList<>();
+    public List<LocalDate> withdr_dates = new ArrayList<>();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     public double CheckBalance() {
@@ -20,15 +21,37 @@ public class BankAccount {
         String str_amount = String.valueOf(amount);
         String[] add = {"Deposit", str_amount, date.format(formatter)};
         history.add(add[0] + " - " + add[1] + " - " + add[2]);
+        System.out.println("Đã gửi " + str_amount + " VND");
     }
     public void Withdraw(double amount, LocalDate date) {
+        if (amount>balance) {
+            System.out.println("Insufficient Balance");
+        }
+        if (n_withdr_monthly(date)>3) {
+            System.out.println("Limit reached");
+        }
         balance = balance - amount;
-        
+        withdr_dates.add(date);
+        String[] add = {"Withdraw", String.valueOf(amount), date.format(formatter)};
+        history.add(add[0] + " - " + add[1] + " - " + add[2]);
+        System.out.println("Đã rút " + String.valueOf(amount) + " VND");
+    }
+    public int n_withdr_monthly(LocalDate date) {
+        int count = 0;
+        Month c_month = date.getMonth();
+        int c_year = date.getYear();
+        for (LocalDate withdrawalDate : withdr_dates) {
+            if (withdrawalDate.getMonth() == c_month && withdrawalDate.getYear() == c_year) {
+                count=count+1;
+            }
+        }
+        return count;
     }
 
+
     public void PrintHistory() {
-        for (i=0; i<history.size(); i++) {
-            System.out.println(history[i]);
+        for (int i=0; i<history.size(); i++) {
+            System.out.println(history.get(i));
         }
     }
 
--- Lab01/bank_acc/src/Main.java
+++ Lab01/bank_acc/src/Main.java
@@ -6,9 +6,16 @@ public class Main {
         acc.name = "Nguyễn Văn A";
         acc.balance = 5000000;
         acc.monthlyInterest = 0.005;
+        LocalDate today = LocalDate.now();
         System.out.println("Tạo tài khoản mới: " + acc.name);
         System.out.println("Số dư ban đầu: " + String.valueOf(acc.balance));
 
-        acc.Deposit(1000000, LocalDate.now());
+        acc.Deposit(1000000, today);
+        acc.Withdraw(500000, today.plusDays(1));
+        acc.Withdraw(500000, today.plusDays(2));
+        acc.Withdraw(500000, today.plusDays(3));
+        acc.Withdraw(500000, today.plusDays(4));
+
+        System.out.println("Số dư hiện tại: " + String.valueOf(acc.balance));
     }
 }
