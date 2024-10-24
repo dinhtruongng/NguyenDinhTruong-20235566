c8ce3f987eba0acfb22b13b018dc86fc519aef01
Truong Ng Dinh <Truong.ND235566@sis.hust.edu.vn>
2024-10-24 09:35:54 +0700

    update

--- Lab01/bank_acc/src/BankAccount.java
+++ Lab01/bank_acc/src/BankAccount.java
@@ -9,13 +9,27 @@ public class BankAccount {
     public double CheckBalance() {
         return balance;
     }
+
     public void Deposit(double amount, String date) {
         balance = balance + amount;
-        history.add("Deposit - " + amount + " - " + date);
+        String str_amount = String.valueOf(amount);
+        String[] add = {"Deposit", str_amount, date};
+        history.add(add[0] + " - " + add[1] + " - " + add[2]);
     }
     public void Withdraw(double amount, String date) {
         balance = balance - amount;
         
+    }
+
+    public void PrintHistory() {
+        for (i=0; i<history.size(); i++) {
+            System.out.println(history[i]);
+        }
+    }
+
+    public void InterestBalance() {
+        double month_int = balance * monthlyInterest;
+        balance = balance + month_int;
     }
 
 }
